ab78bda5269b41fbc705bca7e0387b5302cd242a
Guilherme Ferreira <guilherme.rfs00@gmail.com>
2021-05-01 12:11:28 -0300

    Zuul authorization

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -21,34 +21,31 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-
+	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-
-	@Value("${test.config}")
-	private String testConfig;
-
+	
 	@Autowired
 	private Environment env;
-
+	
 	@Autowired
 	private WorkerRepository repository;
-
+	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
-	}
-
+	}		
+	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
-	}
-
+	}	
+	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
-		Worker worker = repository.findById(id).get();
-		return ResponseEntity.ok(worker);
-	}
+		
+		Worker obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}	
 }
